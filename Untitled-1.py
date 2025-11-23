4e5b1794c04a36a8ca6aa4d7e6ca76f20091e068
andrewdigiacomo27 <andrewdigiacomo27@gmail.com>
2025-11-23 14:08:01 -0600

    attempt 1

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+import statsmodels.formula.api as smf
+import pandas as pd, numpy as np
+
+data = pd.read_csv("https://github.com/dustywhite7/Econ8320/blob/master/AssignmentData/assignment8DAta.csv?raw=true")
